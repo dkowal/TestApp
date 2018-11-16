12fd420f0edad0d4d3ad75d5d3ee521ce91b7b19
Dawid K <dawid.k.kowal@gmail.com>
2018-11-16 11:19:54 +0100

    Create startGitPipeline.groovy

---
+++ vars/startGitPipeline.groovy
@@ -0,0 +1,41 @@
+def call(def gitURL, def dir) {
+    pipeline {
+        agent any
+        stages {
+            stage("checkout from github") {
+                steps {
+                    echo "HELLO WORLD FROM EXPRESSION STAGE"
+                    git "https://github.com/dkowal/ScriptsGroovy.git"
+                }
+
+            }
+
+            stage("Run groovy script on DEV") {
+                steps {
+                    dir("DEV") {
+                        bat "groovy first_script.groovy"
+                    }
+                    echo 'step app'
+                }
+            }
+
+            stage("Run groovy script on TST") {
+                steps {
+                    input("Proceed on TST?")
+                    dir("TST") {
+                        bat "groovy first_script.groovy"
+                    }
+                }
+            }
+
+            stage("Calling library method") {
+                steps {
+                    sayHello "Dawid"
+                    //input("Build second project?")
+                    //build job: 'MavenProject_Test_01', parameters: [[$class: 'StringParameterValue', name: 'Parametr_01', value: "PIERWSZY PARAMETR"],
+                    //                                                [$class: 'StringParameterValue', name: 'Parameter_02', value: "DRUGI PARAMETR"]]
+                }
+            }
+        }
+    }
+}
